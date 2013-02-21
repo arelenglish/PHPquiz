b769798cb67fa45a9c7f4ce147d982317419cfff
Arel English <arelenglish@gmail.com>
2013-02-20 21:16:09 -0500

    first commit

---
+++ quiz.php
@@ -0,0 +1,323 @@
+		<section class="quiz">
+				<p>	Put a description to your quiz here. </p>
+
+				<p>Please respond by selecting the appropriate response according to the following key:</p>
+			<table>
+				<tr>
+					<td class="quiz_guide">UNTRUE</td>
+					<td class="quiz_guide">MODERATELY UNTRUE</td>
+					<td class="quiz_guide">SOMEWHAT UNTRUE</td>
+					<td class="quiz_guide">SOMEWHAT TRUE</td>
+					<td class="quiz_guide">MODERATELY TRUE</td>
+					<td class="quiz_guide">TRUE</td>
+				</tr>
+				<tr>
+					<td class="quiz_guide">1</td>
+					<td class="quiz_guide">2</td>
+					<td class="quiz_guide">3</td>
+					<td class="quiz_guide">4</td>
+					<td class="quiz_guide">5</td>	
+				    <td class="quiz_guide">6</td>
+				</tr>
+			</table>     
+				
+		<form action="?page_id=2" method="post" id="quiz">
+			<div class="questions" id="quiz_title">
+		        <label>Questions</label>
+			</div>
+			<div class="answers" id="quiz_title">
+				1
+				2
+				3
+				4
+				5
+				6
+			</div>
+			<div class="questions">
+		        <label for="question-1">1. Question 1</label>
+			</div>
+			<div class="answers">
+				<input type="radio" name="question-1-answers" value="1" />
+				<input type="radio" name="question-1-answers" value="2" />
+				<input type="radio" name="question-1-answers" value="3" />
+				<input type="radio" name="question-1-answers" value="4" />
+				<input type="radio" name="question-1-answers" value="5" />
+				<input type="radio" name="question-1-answers" value="6" />
+			</div>
+			<div class="questions">
+				<label for="question-2">2. Question 2</label>
+			</div>
+			<div class="answers">
+		    	<input type="radio" name="question-2-answers" value="1" />
+				<input type="radio" name="question-2-answers" value="2" />
+				<input type="radio" name="question-2-answers" value="3" />
+				<input type="radio" name="question-2-answers" value="4" />
+				<input type="radio" name="question-2-answers" value="5" />
+				<input type="radio" name="question-2-answers" value="6" />
+			</div>
+		 	<div class="questions">
+		    	<label for="question-3">3. Question 3</label>
+			</div>
+			<div class="answers">
+		    	<input type="radio" name="question-3-answers" value="1" />
+				<input type="radio" name="question-3-answers" value="2" />
+				<input type="radio" name="question-3-answers" value="3" />
+				<input type="radio" name="question-3-answers" value="4" />
+				<input type="radio" name="question-3-answers" value="5" />
+				<input type="radio" name="question-3-answers" value="6" />
+		    </div>
+		    <div class="questions">
+		    	<label for="question-4">4. Question 4</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-4-answers" value="1" />
+				<input type="radio" name="question-4-answers" value="2" />
+				<input type="radio" name="question-4-answers" value="3" />
+				<input type="radio" name="question-4-answers" value="4" />
+				<input type="radio" name="question-4-answers" value="5" />
+				<input type="radio" name="question-4-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-5">5. Question 5</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-5-answers" value="1" />
+				<input type="radio" name="question-5-answers" value="2" />
+				<input type="radio" name="question-5-answers" value="3" />
+				<input type="radio" name="question-5-answers" value="4" />
+				<input type="radio" name="question-5-answers" value="5" />
+				<input type="radio" name="question-5-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-6">6. Question 6</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-6-answers" value="1" />
+				<input type="radio" name="question-6-answers" value="2" />
+				<input type="radio" name="question-6-answers" value="3" />
+				<input type="radio" name="question-6-answers" value="4" />
+				<input type="radio" name="question-6-answers" value="5" />
+				<input type="radio" name="question-6-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-7">7. Question 7</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-7-answers" value="1" />
+				<input type="radio" name="question-7-answers" value="2" />
+				<input type="radio" name="question-7-answers" value="3" />
+				<input type="radio" name="question-7-answers" value="4" />
+				<input type="radio" name="question-7-answers" value="5" />
+				<input type="radio" name="question-7-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-8">8. Question 8</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-8-answers" value="1" />
+				<input type="radio" name="question-8-answers" value="2" />
+				<input type="radio" name="question-8-answers" value="3" />
+				<input type="radio" name="question-8-answers" value="4" />
+				<input type="radio" name="question-8-answers" value="5" />
+				<input type="radio" name="question-8-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-9">9. Question 9</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-9-answers" value="1" />
+				<input type="radio" name="question-9-answers" value="2" />
+				<input type="radio" name="question-9-answers" value="3" />
+				<input type="radio" name="question-9-answers" value="4" />
+				<input type="radio" name="question-9-answers" value="5" />
+				<input type="radio" name="question-9-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-10">10. Question 10</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-10-answers" value="1" />
+				<input type="radio" name="question-10-answers" value="2" />
+				<input type="radio" name="question-10-answers" value="3" />
+				<input type="radio" name="question-10-answers" value="4" />
+				<input type="radio" name="question-10-answers" value="5" />
+				<input type="radio" name="question-10-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-11">11. Question 11</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-11-answers" value="1" />
+				<input type="radio" name="question-11-answers" value="2" />
+				<input type="radio" name="question-11-answers" value="3" />
+				<input type="radio" name="question-11-answers" value="4" />
+				<input type="radio" name="question-11-answers" value="5" />
+				<input type="radio" name="question-11-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-12">12. Question 12</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-12-answers" value="1" />
+				<input type="radio" name="question-12-answers" value="2" />
+				<input type="radio" name="question-12-answers" value="3" />
+				<input type="radio" name="question-12-answers" value="4" />
+				<input type="radio" name="question-12-answers" value="5" />
+				<input type="radio" name="question-12-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-13">13. Question 13</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-13-answers" value="1" />
+				<input type="radio" name="question-13-answers" value="2" />
+				<input type="radio" name="question-13-answers" value="3" />
+				<input type="radio" name="question-13-answers" value="4" />
+				<input type="radio" name="question-13-answers" value="5" />
+				<input type="radio" name="question-13-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-14">14. Question 14</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-14-answers" value="1" />
+				<input type="radio" name="question-14-answers" value="2" />
+				<input type="radio" name="question-14-answers" value="3" />
+				<input type="radio" name="question-14-answers" value="4" />
+				<input type="radio" name="question-14-answers" value="5" />
+				<input type="radio" name="question-14-answers" value="6" />
+		    </div>
+			<div class="questions">
+		    	<label for="question-15">15. Question 15</label>
+			</div>
+			<div class="answers">
+		        <input type="radio" name="question-15-answers" value="1" />
+				<input type="radio" name="question-15-answers" value="2" />
+				<input type="radio" name="question-15-answers" value="3" />
+				<input type="radio" name="question-15-answers" value="4" />
+				<input type="radio" name="question-15-answers" value="5" />
+				<input type="radio" name="question-15-answers" value="6" />
+		    </div>
+
+		<div class="submit_button">
+			<input type="submit" value="Submit Quiz" />
+		</div><!-- end submit button -->
+		</form>
+		<div class="submit_button">
+		<?php
+
+		    $answer1 = $_POST['question-1-answers'];
+		    $answer2 = $_POST['question-2-answers'];
+		    $answer3 = $_POST['question-3-answers'];
+		    $answer4 = $_POST['question-4-answers'];
+		    $answer5 = $_POST['question-5-answers'];
+			$answer6 = $_POST['question-1-answers'];
+		    $answer7 = $_POST['question-2-answers'];
+		    $answer8 = $_POST['question-3-answers'];
+		    $answer9 = $_POST['question-4-answers'];
+		    $answer10 = $_POST['question-5-answers'];
+			$answer11 = $_POST['question-1-answers'];
+		    $answer12 = $_POST['question-2-answers'];
+		    $answer13 = $_POST['question-3-answers'];
+		    $answer14 = $_POST['question-4-answers'];
+		    $answer15 = $_POST['question-5-answers'];
+
+		    $result = 0;
+
+		    if ($answer1 == "1") { $result++; }
+			if ($answer1 == "2") { $result = i + 2; }
+			if ($answer1 == "3") { $result = i + 3; }
+			if ($answer1 == "4") { $result = i + 4; }
+			if ($answer1 == "5") { $result = i + 5; }
+			if ($answer1 == "6") { $result = i + 6; }
+		    if ($answer2 == "1") { $result++; }
+		    if ($answer2 == "2") { $result = i + 2; }
+		    if ($answer2 == "3") { $result = i + 3; }
+			if ($answer2 == "4") { $result = i + 4; }
+			if ($answer2 == "5") { $result = i + 5; }
+			if ($answer2 == "6") { $result = i + 6; }
+			if ($answer3 == "1") { $result++; }
+		    if ($answer3 == "2") { $result = i + 2; }
+		    if ($answer3 == "3") { $result = i + 3; }
+			if ($answer3 == "4") { $result = i + 4; }
+			if ($answer3 == "5") { $result = i + 5; }
+			if ($answer3 == "6") { $result = i + 6; }
+			if ($answer4 == "1") { $result++; }
+		    if ($answer4 == "2") { $result = i + 2; }
+		    if ($answer4 == "3") { $result = i + 3; }
+			if ($answer4 == "4") { $result = i + 4; }
+			if ($answer4 == "5") { $result = i + 5; }
+			if ($answer4 == "6") { $result = i + 6; }
+			if ($answer5 == "1") { $result++; }
+		    if ($answer5 == "2") { $result = i + 2; }
+		    if ($answer5 == "3") { $result = i + 3; }
+			if ($answer5 == "4") { $result = i + 4; }
+			if ($answer5 == "5") { $result = i + 5; }
+			if ($answer5 == "6") { $result = i + 6; }
+			if ($answer6 == "1") { $result++; }
+		    if ($answer6 == "2") { $result = i + 2; }
+		    if ($answer6 == "3") { $result = i + 3; }
+			if ($answer6 == "4") { $result = i + 4; }
+			if ($answer6 == "5") { $result = i + 5; }
+			if ($answer6 == "6") { $result = i + 6; }
+			if ($answer7 == "1") { $result++; }
+		    if ($answer7 == "2") { $result = i + 2; }
+		    if ($answer7 == "3") { $result = i + 3; }
+			if ($answer7 == "4") { $result = i + 4; }
+			if ($answer7 == "5") { $result = i + 5; }
+			if ($answer7 == "6") { $result = i + 6; }
+			if ($answer8 == "1") { $result++; }
+		    if ($answer8 == "2") { $result = i + 2; }
+		    if ($answer8 == "3") { $result = i + 3; }
+			if ($answer8 == "4") { $result = i + 4; }
+			if ($answer8 == "5") { $result = i + 5; }
+			if ($answer8 == "6") { $result = i + 6; }
+			if ($answer9 == "1") { $result++; }
+		    if ($answer9 == "2") { $result = i + 2; }
+		    if ($answer9 == "3") { $result = i + 3; }
+			if ($answer9 == "4") { $result = i + 4; }
+			if ($answer9 == "5") { $result = i + 5; }
+			if ($answer9 == "6") { $result = i + 6; }
+			if ($answer10 == "1") { $result++; }
+		    if ($answer10 == "2") { $result = i + 2; }
+		    if ($answer10 == "3") { $result = i + 3; }
+			if ($answer10 == "4") { $result = i + 4; }
+			if ($answer10 == "5") { $result = i + 5; }
+			if ($answer10 == "6") { $result = i + 6; }
+			if ($answer11 == "1") { $result++; }
+		    if ($answer11 == "2") { $result = i + 2; }
+		    if ($answer11 == "3") { $result = i + 3; }
+			if ($answer11 == "4") { $result = i + 4; }
+			if ($answer11 == "5") { $result = i + 5; }
+			if ($answer11 == "6") { $result = i + 6; }
+			if ($answer12 == "1") { $result++; }
+		    if ($answer12 == "2") { $result = i + 2; }
+		    if ($answer12 == "3") { $result = i + 3; }
+			if ($answer12 == "4") { $result = i + 4; }
+			if ($answer12 == "5") { $result = i + 5; }
+			if ($answer12 == "6") { $result = i + 6; }
+			if ($answer13 == "1") { $result++; }
+		    if ($answer13 == "2") { $result = i + 2; }
+		    if ($answer13 == "3") { $result = i + 3; }
+			if ($answer13 == "4") { $result = i + 4; }
+			if ($answer13 == "5") { $result = i + 5; }
+			if ($answer13 == "6") { $result = i + 6; }
+			if ($answer14 == "1") { $result++; }
+		    if ($answer14 == "2") { $result = i + 2; }
+		    if ($answer14 == "3") { $result = i + 3; }
+			if ($answer14 == "4") { $result = i + 4; }
+			if ($answer14 == "5") { $result = i + 5; }
+			if ($answer14 == "6") { $result = i + 6; }
+			if ($answer15 == "1") { $result++; }
+		    if ($answer15 == "2") { $result = i + 2; }
+		    if ($answer15 == "3") { $result = i + 3; }
+			if ($answer15 == "4") { $result = i + 4; }
+			if ($answer15 == "5") { $result = i + 5; }
+			if ($answer15 == "6") { $result = i + 6; }
+		     $result=array(0=> "$answer1", 1=> "$answer2", 2=> "$answer3", 3=> "$answer4", 4=> "$answer5", 5=> "$answer6", 6=> "$answer7", 7=> "$answer8", 8=> "$answer9", 9=> "$answer10", 10=> "$answer11", 11=> "$answer12", 12=> "$answer13", 13=> "$answer14", 14=> "$answer15"); 
+	
+	echo "Your score is: "; echo array_sum($result);
+?>
+
+		</div>
+		</section><!-- END QUIZ -->
---
+++ readme.markdown
@@ -0,0 +1,12 @@
+## A simple PHP quiz. 
+
+If you want to implement this quiz into your Wordpress site or other PHP site, just follow the instructions below:
+
+1. Download Source Code
+2. Paste code from quiz.php into your php file where you want the quiz to show up
+3. Paste the css styles from styles.css into your main css file, or import the styles.css file
+4 edit the questions and use the quiz! 
+
+I made this quiz in an afternoon, so don't expect it to be the best, or the leanest code. If you need something simple, feel free to use it, hack it, whatever you want. 
+
+Finally, if you want to make improvements, feel free to fork me. 
